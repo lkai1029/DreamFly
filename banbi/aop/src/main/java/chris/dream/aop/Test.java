a53d187f461466fdb49e787eaf53ddfae0dfac9d
Chris <lkai1029@163.com>
2018-08-14 10:59:35 +0800

    删除原来eclipse中的多余文件

---
+++ banbi/aop/src/main/java/chris/dream/aop/Test.java
@@ -0,0 +1,12 @@
+package chris.dream.aop;
+
+/**
+ * @author Chris
+ * @describe: <p>...</p>
+ * @date 下午 7:47 2018-08-13
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
